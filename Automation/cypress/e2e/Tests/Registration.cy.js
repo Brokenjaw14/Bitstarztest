40bee1106f316246242fedac63d5077e7f1c6cab
Brokenjaw14 <112245536+Brokenjaw14@users.noreply.github.com>
2022-08-29 11:11:33 +0200

    Update Registration.cy.js

--- Automation/cypress/e2e/Tests/Registration.cy.js
+++ Automation/cypress/e2e/Tests/Registration.cy.js
@@ -10,7 +10,7 @@ describe('Cy Visit', function() {
     it('test visit method', function() {
       cy.visit('https://test.bitstarz.com/')
       cy.get('[data-cy="signup_btn_header"]').click()
-      cy.get('[data-cy="cy-signup-modal-email"]').type(email)
+      cy.get('[data-cy="cy-signup-modal-email"]').type(`testingpurpose221+${timestamp}@gmail.com`)
       cy.get('[data-cy="cy-signup-modal-username"]').click().type(username)
       cy.get('[data-cy="cy-signup-modal-password"]').click().type(password)
       cy.get('[data-cy="cy-signup-modal-currency-dropdown"] > .bs-c-deposit__input').click()
